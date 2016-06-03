4a454c671baec1594a046b1c8c40dbcf660ef3ec
Li Hongyin <hongyin163@163.com>
2016-06-03 15:28:24 +0800

    update

--- test.js
+++ test.js
@@ -1,22 +1,22 @@
 var Jsonnet = require('./index.js');
-var os=require('os');
+var os = require('os');
+var iconv = require('iconv-lite');
+var json = { name: "中华人民共和国", age: 20 };
+// Jsonnet.transform(JSON.stringify(json), function (err, data) {
+// 	console.log(err);
+// 	console.log(data);
+// });
 
-var json={name:"lhy",age:20};
-Jsonnet.eval(JSON.stringify(json),function (err,data) {	
-	console.log(err);
-	console.log(data);
-});
-
-Jsonnet.evalFile('./data.json',function (err,data) {
-	console.log(err);
-	console.log(data);
-});
+// Jsonnet.evalFile('./data.json',function (err,data) {
+// 	console.log(err);
+// 	console.log(data);
+// });
 
 
-Jsonnet.transform(JSON.stringify(json),function (err,data) {
-	console.log(err);
-	console.log(data);
-});
+// Jsonnet.transform(JSON.stringify(json),function (err,data) {
+// 	console.log(err);
+// 	console.log(data);
+// });
 
 Jsonnet.transformFile('./data.json',function (err,data) {
 	console.log(err);
